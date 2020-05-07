0f78b9cde24207819f0e30f6615244822ada2452
Matt Bailey <matt.bailey@elucidat.com>
2020-05-07 15:36:55 +0100

    catch errors in auth

--- src/contollers/auth.controller.js
+++ src/contollers/auth.controller.js
@@ -14,6 +14,10 @@ module.exports = {
                 } else {
                     callback(true, null)
                 }
+            })
+            .catch(() => {
+                console.log('auth error')
+                callback(true, null)
             });
     }
 }
